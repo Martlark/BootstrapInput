44d905d77a2273199d43e21adcb91f6164990594
Martlark <mr_andrew_d_rowe@hotmail.com>
2021-10-10 13:17:54 +1100

    updates

--- webpack.config.js
+++ webpack.config.js
@@ -45,7 +45,8 @@ module.exports = (env) => {
         }, devServer: {
             contentBase: path.join(__dirname, 'dist'),
             compress: true,
-            port: 9000
+            port: 9000,
+            index: 'index.html'
         },
         optimization: {
             minimize: false
